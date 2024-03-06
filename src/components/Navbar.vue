f003ec2964715d91680a3e9ff7561f0f1164714c
Jmimiding4104@gmail.com <Jmimiding4104>
2024-03-07 01:38:10 +0800

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,50 @@
+<template>
+  <nav class="navbar navbar-expand-md navbar-light bg-light p-3">
+    <div class="container-fluid">
+      <router-link class="nav-link fw-bold fs-3" to="/"><i class="bi bi-arrow-right-square"></i>LIU MIN YAO</router-link>
+      <router-link class="nav-link fw-bold fs-3" to="/about"><i class="bi bi-arrow-right-square"></i>LIU MIN YAO</router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-md-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdown"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Dropdown
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="#">Action11</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><hr class="dropdown-divider" /></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled">Disabled</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
---
+++ src/main.js
@@ -0,0 +1,18 @@
+import './assets/style.scss'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+ 
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')

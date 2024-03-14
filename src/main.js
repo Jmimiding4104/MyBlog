18bcceabfe9755cea3bb42c6d6e6018eb671e92c
test <test@gmail.com>
2024-03-14 16:58:07 +0800

    update

--- src/main.js
+++ src/main.js
@@ -9,11 +9,15 @@ import "bootstrap";
 import App from './App.vue'
 import router from './router'
 
+import Swiper from 'swiper';
+
 const app = createApp(App)
+
+router.afterEach((to, from, next) => {
+	window.scrollTo(0, 0);
+});
 
 app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
-
-console.log(123)
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,1 +1,40 @@
-<template>contact</template>
+<template>
+  <swiper :pagination="pagination" :navigation="true" :modules="modules" class="mySwiper">
+    <swiper-slide>Slide 1</swiper-slide>
+    <swiper-slide>Slide 2</swiper-slide><swiper-slide>Slide 3</swiper-slide>
+    <swiper-slide>Slide 4</swiper-slide><swiper-slide>Slide 5</swiper-slide>
+    <swiper-slide>Slide 6</swiper-slide><swiper-slide>Slide 7</swiper-slide>
+    <swiper-slide>Slide 8</swiper-slide><swiper-slide>Slide 9</swiper-slide>
+  </swiper>
+</template>
+
+<script>
+  // Import Swiper Vue.js components
+  import { Swiper, SwiperSlide } from 'swiper/vue';
+
+  // Import Swiper styles
+  import 'swiper/css';
+
+  import 'swiper/css/pagination';
+
+  // import required modules
+  import { Pagination } from 'swiper/modules';
+
+  export default {
+    components: {
+      Swiper,
+      SwiperSlide,
+    },
+    setup() {
+      return {
+        pagination: {
+          clickable: true,
+          renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (index + 1) + '</span>';
+          },
+        },
+        modules: [Pagination],
+      };
+    },
+  };
+</script>

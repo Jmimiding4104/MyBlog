53e70b8d6223728b66c92bbd7fe334a15b9fc6c5
Jmimiding4104@gmail.com <Jmimiding4104>
2024-03-18 01:01:41 +0800

    可以 build

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
+import 'dotenv/config'
+
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
-
+console.log(process.env.REPOSITORY_NAME);
 // https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? `/${process.env.REPOSITORY_NAME}/` : '/',
